aaf4fc305231c10002c1287b50054a4b69e99c2b
Side_project_VM <ozbrancov@gmail.com>
2015-07-28 22:59:09 -0700

    first working view with django user authentication

--- gateway/hangout/views.py
+++ gateway/hangout/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	template = 'home.html'
-	context = {}
+	if request.user.is_authenticated():
+		username_is = "oliverrrrr"
+		context = {"username_is": username_is}
+	else:
+		context = {"username_is": request.user}
+
+	template = 'base.html'
+	
 	return render(request, template, context)
